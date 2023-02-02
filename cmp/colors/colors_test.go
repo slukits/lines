3665de2b89307ff02f0b0a8286a2d163f56a285c
Stephan Lukits <stephan.lukits@gmail.com>
2023-02-02 11:50:13 +0200

    add: types of color ranges and their values
    Lines wants to support the color ranges
        - Monochrome
        - Eight system colors
        - Linux system colors
        - Sixteen system colors
        - ANSI colors (256)
        - True color
    and provide for each of these ranges DropDown-selections to ultimately
    choose a style with fore- and background color.  To make it easy to
    provide consistent style selectors for the various color ranges each
    color range has its own typing.

--- cmp/colors/colors_test.go
+++ cmp/colors/colors_test.go
@@ -18,24 +18,24 @@ func (s *Monochrome) SetUp(t *T) { t.Parallel() }
 func (s *Monochrome) Has_two_colors(t *T) { t.Eq(2, len(MonoColors)) }
 
 func (s *Monochrome) Offers_one_foreground_to_choose_from(t *T) {
-	t.Eq(1, len(MonoForeground(Black)))
-	t.Eq(1, len(MonoForeground(White)))
+	t.Eq(1, len(MonoForeground(BlackM)))
+	t.Eq(1, len(MonoForeground(WhiteM)))
 }
 
 func (s *Monochrome) Offers_white_foreground_to_black_background(t *T) {
-	t.Eq(lines.White, MonoForeground(Black)[0].FG())
+	t.Eq(lines.White, MonoForeground(BlackM)[0].FG())
 }
 
 func (s *Monochrome) Offers_black_foreground_to_white_background(t *T) {
-	t.Eq(lines.Black, MonoForeground(White)[0].FG())
+	t.Eq(lines.Black, MonoForeground(WhiteM)[0].FG())
 }
 
 func (s *Monochrome) Offers_white_background_to_black_foreground(t *T) {
-	t.Eq(lines.White, MonoBackground(Black)[0].BG())
+	t.Eq(lines.White, MonoBackground(BlackM)[0].BG())
 }
 
 func (s *Monochrome) Offers_black_background_to_white_foreground(t *T) {
-	t.Eq(lines.Black, MonoBackground(White)[0].BG())
+	t.Eq(lines.Black, MonoBackground(WhiteM)[0].BG())
 }
 
 func TestMonochrome(t *testing.T) {
